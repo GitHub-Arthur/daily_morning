44c6b70aea9bc7e9ed65a6192a123a701a144cac
1911080146chenjiongyu <1156341440@qq.com>
2022-08-27 21:55:12 +0800

    add ]

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def get_words_pyq():
   words = requests.get("https://api.shadiao.pro/pyq")
   if words.status_code != 200:
     return get_words_pyq()
-  return words.json()['data']['text'
+  return words.json()['data']['text']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
